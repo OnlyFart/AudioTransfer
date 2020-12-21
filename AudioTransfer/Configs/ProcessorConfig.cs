6741c37f03b07ba7d45349ec08bc98ce325c37c5
oleg.koloskov <oleg.koloskov@ingate.ru>
2020-12-21 17:40:13 +0300

    Выкинул еще одну ненужную либу

--- AudioTransfer/Configs/ProcessorConfig.cs
+++ AudioTransfer/Configs/ProcessorConfig.cs
@@ -1,8 +1,9 @@
 namespace AudioTransfer.Configs {
     public interface IProcessorConfig {
         /// <summary>
-        /// Коллисество потоков для обработки файлов
+        /// Количество потоков для обработки файлов
         /// </summary>
         int ThreadsCount { get; set; }
+        
     }
 }
